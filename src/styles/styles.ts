63ecce364078e143b9ca5a8ba014381413b88bf4
RochelAngelo <jrochelangelo@gmail.com>
2023-07-19 23:00:59 -0300

    correção para deploy

---
+++ src/styles/styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  /* Estilos para o container */
+`;
+
+export const Item = styled.div`
+  /* Estilos para o item */
+`;
+
+export const Image = styled.img`
+  /* Estilos para a imagem */
+`;
+
+export const Description = styled.h3`
+  /* Estilos para a descrição */
+`;
+
+// Adicione a exportação vazia
+export {};
